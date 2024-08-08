bd76fc48315ba53af2684fe0b32e99358daea581
kadirtikil <kadirtikil@gmail.com>
2024-08-08 12:59:01 +0200

    map builder works. duplicates got to be handles. furthermore, lists have to be possible

--- src/main/scala/JsonObject.scala
+++ src/main/scala/JsonObject.scala
@@ -2,8 +2,7 @@ package scala
 
 import scala.annotation.tailrec
 
-// traits:
-// a json value can consist of different datatype. so 
+// Traits representing different JSON value types
 sealed trait JsonValue 
 case class JsonString(jsonString: String) extends JsonValue
 case class JsonInt(jsonInt: Int) extends JsonValue
@@ -11,29 +10,48 @@ case class JsonFloat(jsonFloat: Float) extends JsonValue
 case class JsonDouble(jsonDouble: Double) extends JsonValue
 case class JsonBoolean(jsonBoolean: Boolean) extends JsonValue
 case class JsonList(jsonList: List[JsonValue]) extends JsonValue
+case class JsonNull(jsonNull: String = "null") extends JsonValue // Providing a default value for JsonNull
 
 class FunctionalJsonObject(listOfTokens: List[String]) {
 
-  def functionalMapBuilderForJson() : Map[String,  JsonValue] = {
+  def functionalMapBuilderForJson(): Map[String, JsonValue] = {
     @tailrec
     def helper(parsingTokens: List[String], mapOfJsonVals: Map[String, JsonValue]): Map[String, JsonValue] = {
-      if(parsingTokens.isEmpty) mapOfJsonVals
+      if (parsingTokens.isEmpty) mapOfJsonVals
       else {
         parsingTokens.head match {
-          case s if (s.startsWith("\"")) => {
-            // val restListForParsing = something that returns the list after adding key and value 
-          }
-          case s if (s.startsWith(":")) => mapOfJsonVals
-          case s if (s.startsWith(",")) =>mapOfJsonVals 
-          case s if (s.startsWith("[")) =>mapOfJsonVals
-          case s if (s.startsWith("}")) => mapOfJsonVals
-          case _ => helper(parsingTokens.tail, mapOfJsonVals) 
-          
+          case s if s.startsWith("\"") =>
+            // Key found:
+            val keyOfVal = s.stripPrefix("\"").stripSuffix("\"")
+            // Check for the next element (no list yet):
+            parsingTokens.tail match {
+              case ":" :: value :: tail =>
+                val valueForKey = valueTyper(value)
+                helper(tail, mapOfJsonVals + (keyOfVal -> valueForKey))
+              case _ => helper(parsingTokens.tail, mapOfJsonVals)
+            }
+          case _ => helper(parsingTokens.tail, mapOfJsonVals)
         }
       }
     }
-    
+
     helper(listOfTokens, Map())
   }
 
+  // Helper function to return either an element for a key or a list.
+  def valueTyper(token: String): JsonValue = {
+    token match {
+      case s if s.startsWith("\"") => JsonString(s.stripPrefix("\"").stripSuffix("\""))
+      case s if s == "true" => JsonBoolean(true)
+      case s if s == "false" => JsonBoolean(false)
+      case s if helperIsNumber(s) =>
+        if (s.contains(".")) JsonDouble(s.toDouble)
+        else JsonInt(s.toInt)
+      case _ => JsonNull() // Default case to handle unexpected tokens
+    }
+  }
+
+  def helperIsNumber(potentialNum: String): Boolean = {
+    potentialNum.forall(c => c.isDigit || c == '.') && potentialNum.nonEmpty
+  }
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,16 @@
 package scala
 
 @main def hello(): Unit = {
-  val jsonString = """{"key" : "value", "some list" : [1, 2, 3], "number" : 123.321, "flag" : "true", "flag" : false, "test" : 123.456789}""" 
-  val temp = new FunctionalJsonTokenizer(jsonString)
-  
+  val jsonString = """{"key" : "value", "some list" : 123, "number" : 123.321, "flag" : true, "flag" : false, "test" : 123.456789}""" 
+  val temp = new FunctionalJsonTokenizer(jsonString) 
   val tokens = temp.tokenizer()
 
+  val parser = new FunctionalJsonObject(tokens)
+  val result = parser.functionalMapBuilderForJson()
 
-  print(tokens)
+
+
+  print(result)
 }
 
 
